cd66e4239155dc10c15db55eed542be94674aeed
AxelAlvardo <00216022@uca.edu.sv>
2024-07-24 19:02:07 -0600

    :rocket: Server

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ import dotenv from 'dotenv'
 dotenv.config();
 const port = process.env.PORT;
 
-server.listen(port, ()=> {
+server.listen( port, ()=> {
     console.log(colors.cyan(`Servidor iniciado en el puerto ${port}`));
 })
